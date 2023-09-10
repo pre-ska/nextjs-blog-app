1664678bdd232e8b282ce69a5d27a3071be17e8d
preska <wolfwar@outlook.com>
2023-09-10 15:07:33 +0200

    blog and category page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>wolf&apos;s blog</div>
+      <div className={styles.logo}>Troll&apos;s blog</div>
       <div className={styles.links}>
         <ThemeToggle />
         <Link href="/" className={styles.link}>
