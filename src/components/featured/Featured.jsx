272516a6530503e9e6de38fbd8659d426a802b82
preska <wolfwar@outlook.com>
2023-09-10 10:23:12 +0200

    featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,8 +6,24 @@ const Featured = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, lama dev here!</b> Discover my stories and creative ideas.
+        <b>Hey here!</b> Discover stories and creative ideas.
       </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="hero" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+            consequuntur, quisquam, quisquam. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Illum nobis fuga accusantium totam
+            esse aut, ipsum aliquid voluptatem facere nulla quo distinctio
+            voluptas vel, hic maxime, quis in nemo? Incidunt?
+          </p>
+          <button className={styles.button}>Read more</button>
+        </div>
+      </div>
     </div>
   );
 };
